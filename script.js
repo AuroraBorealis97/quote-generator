b85a4ae9e0838f16eb84ba3fbef3174044aa5947
Alicia <aliciatiernan97@gmail.com>
2021-09-07 21:52:15 -0500

    update style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let div = document.querySelector('div');
 let button = document.querySelector('button');
-let qoute = document.querySelector('q');
+let qoute = document.querySelector('p');
 let h5 = document.querySelector('h5');
 
 
@@ -10,9 +10,8 @@ button.addEventListener('click', generate);
 async function generate() {
     const res = await fetch('https://type.fit/api/quotes');
     const data = await res.json();
-    
     let n = Math.floor(Math.random() * 1643);
-    console.log(data[n])
-    qoute.innerHTML = data[n].text;
+    
+    qoute.innerHTML ='"' + data[n].text +'"';
     h5.innerHTML = "-" + " " + data[n].author;
 };
